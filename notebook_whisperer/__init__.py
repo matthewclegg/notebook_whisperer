ffe161c57e5b7c9b16563800c800282729d5a78a
Matthew Clegg <matthewcleggphd@gmail.com>
2023-07-29 18:17:10 -0700

    WIP

---
+++ notebook_whisperer/__init__.py
@@ -0,0 +1,3 @@
+from .notebook_whisperer import ask
+from .notebook_whisperer import assist
+from .notebook_whisperer import start_helper
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='notebook_whisperer',
+    version='0.0.1',
+    packages=['notebook_whisperer'],
+)
